e7740685df859564e150f5ec2b009524dbaee5f8
Tarquiniy <airserfer@rambler.ru>
2025-09-19 13:41:09 +0500

    c

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views_proxy import wordpress_posts_proxy
 
 urlpatterns = [
-    #path('wordpress/posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
-    path('posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
+    path('wordpress/posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
+    #path('posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
 ]
