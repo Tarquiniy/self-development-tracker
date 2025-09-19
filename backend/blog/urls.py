2468ab4d805962409d8192d6b592472722b2d04a
Tarquiniy <airserfer@rambler.ru>
2025-09-19 13:44:51 +0500

    c

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,5 +3,5 @@ from .views_proxy import wordpress_posts_proxy
 
 urlpatterns = [
     path('wordpress/posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
-    #path('posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
+    path('posts/', wordpress_posts_proxy, name='wordpress_posts_proxy'),
 ]
