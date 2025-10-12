581ebe388dc929fe724a5954fce2558eb07175fc
Tarquiniy <airserfer@rambler.ru>
2025-10-12 17:53:52 +0500

    с

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, "backend", "static"),]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blog", "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -2,12 +2,15 @@
 WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Добавьте путь к проекту в sys.path
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_path not in sys.path:
+    sys.path.append(project_path)
 
 from django.core.wsgi import get_wsgi_application
 
