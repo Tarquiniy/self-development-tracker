866b7f6e15c05762c6ad3d5d72d72682c429ade7
Tarquiniy <airserfer@rambler.ru>
2025-10-14 15:37:52 +0500

    c

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
-    name = "backend.users"
-    label = "users"  # важно: чтобы AUTH_USER_MODEL = "users.CustomUser" работал
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+    def ready(self):
+        import users.signals
--- backend/users/signals.py
+++ backend/users/signals.py
@@ -3,13 +3,11 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import UserProfile
 
-
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     """Создает профиль пользователя автоматически при создании нового пользователя"""
     if created:
-        UserProfile.objects.get_or_create(user=instance)
-
+        UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
