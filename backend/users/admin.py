3ceeabe4f848e2c29e564fed8f88d84099e31be7
Tarquiniy <airserfer@rambler.ru>
2025-12-08 11:42:35 +0500

    c

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,18 +1,19 @@
 # backend/users/admin.py
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile
 
 
-# -----------------------------
-# INLINE для профиля пользователя
-# -----------------------------
+# Inline для отображения/редактирования профиля прямо на странице CustomUser
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
+    fk_name = "user"            # поле в UserProfile, ссылающееся на CustomUser
     can_delete = False
-    fk_name = "user"
-
-    # Поля, которые будут отображаться в инлайн-форме
+    verbose_name = "User profile"
+    verbose_name_plural = "User profile"
+    extra = 1                   # показывает пустую форму для создания профиля, если его нет
+    max_num = 1                 # всего максимум 1 профиль на пользователя
     fields = (
         "subscription_active",
         "subscription_expires",
@@ -20,62 +21,28 @@ class UserProfileInline(admin.StackedInline):
     )
 
 
-# -----------------------------
-# Админка CustomUser
-# -----------------------------
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = (
-        'email',
-        'username',
-        'supabase_uid',
-        'registration_method',
-        'is_staff',
+        "email",
+        "username",
+        "supabase_uid",
+        "registration_method",
+        "is_staff",
     )
-    list_filter = ('registration_method', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'username', 'supabase_uid')
+    list_filter = ("registration_method", "is_staff", "is_superuser")
+    search_fields = ("email", "username", "supabase_uid")
 
     fieldsets = UserAdmin.fieldsets + (
-        ('Дополнительно', {
-            'fields': ('supabase_uid', 'registration_method'),
-        }),
+        ("Additional Info", {"fields": ("supabase_uid", "registration_method")}),
     )
 
-    # Добавляем инлайн профиль
-    inlines = [UserProfileInline]
+    inlines = [UserProfileInline]   # <-- вот тут добавлен inline
 
 
-# -----------------------------
-# Админка UserProfile (отдельная)
-# -----------------------------
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = (
-        'user',
-        'subscription_active',
-        'subscription_expires',
-        'tables_limit',
-    )
-    search_fields = ('user__email', 'user__username')
-
-    fields = (
-        'user',
-        'subscription_active',
-        'subscription_expires',
-        'tables_limit',
-    )
-
-    readonly_fields = ('user',)
-
-
-# -----------------------------
-# Сигналы создания профиля
-# -----------------------------
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.conf import settings
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.get_or_create(user=instance)
+    list_display = ("user", "subscription_active", "subscription_expires", "tables_limit")
+    search_fields = ("user__email", "user__username")
+    fields = ("user", "subscription_active", "subscription_expires", "tables_limit")
+    readonly_fields = ("user",)
